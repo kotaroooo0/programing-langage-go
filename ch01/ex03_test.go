4aea02d4be18fe020e3ef1a575070bba5ca51ce4
kotaroooo0 <k33asby@gmail.com>
2020-09-02 16:16:50 +0900

    wip: bench

--- ch01/ex03_test.go
+++ ch01/ex03_test.go
@@ -10,9 +10,16 @@ var s = randStringSlice(10, 10000)
 func BenchmarkEchoFor(b *testing.B) {
 	echoFor(s)
 }
+
+// benchmark
+// 0.123 ns/op	       0 B/op	       0 allocs/op
+
 func BenchmarkEchoJoin(b *testing.B) {
 	echoJoin(s)
 }
+
+// benchmark
+// 0.00203 ns/op	       0 B/op	       0 allocs/op
 
 var rs1Letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
