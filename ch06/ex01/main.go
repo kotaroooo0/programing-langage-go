fdd0d9204343041640e49ae8f3353478971f3c92
Kotaro Adachi <k33asby@gmail.com>
2020-10-25 23:25:50 +0900

    wip

--- ch06/ex01/main.go
+++ ch06/ex01/main.go
@@ -26,3 +26,38 @@ func (s *IntSet) UnionWith(t *IntSet) {
 		}
 	}
 }
+
+func (s *IntSet) Len() int {
+	l := 0
+	for _, word := range s.words {
+		l += popCount(word)
+	}
+	return l
+}
+
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if word > len(s.words) {
+		return
+	}
+	s.words[word] &= ^(1<<bit - 1)
+}
+
+func (s *IntSet) Clear() {
+	s.words = make([]uint64, 0)
+}
+
+func (s *IntSet) Copy() *IntSet {
+	w := make([]uint64, len(s.words))
+	copy(w, s.words)
+	return &IntSet{w}
+}
+
+func popCount(x uint64) int {
+	sum := 0
+	for x > 0 {
+		x = x & (x - 1)
+		sum++
+	}
+	return sum
+}
