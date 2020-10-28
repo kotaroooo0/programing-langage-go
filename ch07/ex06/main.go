a8013c513748d6e97059d8602c5f539ebce0d1fd
Kotaro Adachi <k33asby@gmail.com>
2020-10-28 23:11:01 +0900

    wip

--- ch07/ex06/main.go
+++ ch07/ex06/main.go
@@ -24,6 +24,9 @@ func (f *celsiusFlag) Set(s string) error {
 	case "F", "℉":
 		f.Celsius = FToC(Fahrenheit(value))
 		return nil
+	case "K":
+		f.Celsius = KToC(Kelvin(value))
+		return nil
 	}
 	return fmt.Errorf("invalid temperature %q", s)
 }
@@ -58,3 +61,5 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
 func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
+func KToC(k Kelvin) Celsius { return Celsius(k + 273.15) }
