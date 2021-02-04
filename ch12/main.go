57d6fb7a08ac5786ad56116594dba2efb5d29e2f
Kotaro Adachi <k33asby@gmail.com>
2021-02-05 07:22:19 +0900

    slide

--- ch12/main.go
+++ ch12/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func display(path string, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Invalid:
+		fmt.Printf("%s = invalid\n", path)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			display(fmt.Sprintf("%s[%d]", path, i), v.Index(i))
+		}
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			display(fieldPath, v.Field(i))
+		}
 
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kotaroooo0/programing-language-go
 
 go 1.15
 
-require golang.org/x/exp/errors v0.0.0-20201229011636-eab1b5eb1a03
+require (
+	golang.org/x/exp/errors v0.0.0-20201229011636-eab1b5eb1a03
+	golang.org/x/tools v0.1.0 // indirect
+)
